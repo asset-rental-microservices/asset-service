b00b0e03522a516ca1cea4bba8a461525444213a
sarthak <msarthak@thoughtworks.com>
2019-04-28 18:52:08 +0530

    Sarthak | Adds eureka client

--- src/main/kotlin/org/rentalhouse/assets/Application.kt
+++ src/main/kotlin/org/rentalhouse/assets/Application.kt
@@ -2,10 +2,12 @@ package org.rentalhouse.assets
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
 @SpringBootApplication
 @EnableWebMvc
+@EnableEurekaClient
 class Application
 
 fun main() {
