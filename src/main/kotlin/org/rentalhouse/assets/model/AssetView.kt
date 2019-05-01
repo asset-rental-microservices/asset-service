ad870fac67fb30b5c9c08f0578acae049ea72e7b
sarthak <msarthak@thoughtworks.com>
2019-05-01 20:38:08 +0530

    Sarthak | Checked asset status in integration test

--- src/main/kotlin/org/rentalhouse/assets/model/AssetView.kt
+++ src/main/kotlin/org/rentalhouse/assets/model/AssetView.kt
@@ -2,11 +2,14 @@ package org.rentalhouse.assets.model
 
 import org.rentalhouse.assets.entity.Asset
 
-class AssetView private constructor(val id: String, val plotIdentifier: String, val address: Address) {
+class AssetView private constructor(val id: String,
+                                    val status: Status,
+                                    val plotIdentifier: String,
+                                    val address: Address) {
 
     companion object {
         fun fromAssert(asset: Asset): AssetView {
-            return AssetView(asset.id, asset.plotIdentifier, Address.fromAddress(asset.address))
+            return AssetView(asset.id, asset.status, asset.plotIdentifier, Address.fromAddress(asset.address))
         }
     }
 }
--- src/test/kotlin/org/rentalhouse/assets/controller/AssetControllerIntegrationTest.kt
+++ src/test/kotlin/org/rentalhouse/assets/controller/AssetControllerIntegrationTest.kt
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.rentalhouse.assets.fixture.asset
+import org.rentalhouse.assets.model.Status
 import org.rentalhouse.assets.repository.AssetRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
@@ -49,6 +50,7 @@ class AssetControllerIntegrationTest {
         val addedAsset = assetRepository.findAll().first()
 
         assertThat(addedAsset.id).isNotNull()
+        assertThat(addedAsset.status).isEqualTo(Status.AVAILABLE)
         assertThat(addedAsset.plotIdentifier).isEqualTo("b/401")
         assertThat(addedAsset.street()).isEqualTo("Magarpatta")
         assertThat(addedAsset.city()).isEqualTo("Pune")
@@ -73,10 +75,11 @@ class AssetControllerIntegrationTest {
 
         mockMvc.perform(MockMvcRequestBuilders.get("/v1/assets/$id"))
             .andExpect(MockMvcResultMatchers.status().isOk)
-            .andExpect(MockMvcResultMatchers.jsonPath("$.plotIdentifier", Matchers.`is`("B/401")))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.address.street", Matchers.`is`("John's Street")))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.address.city", Matchers.`is`("Pune")))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.plotIdentifier",  Matchers.`is`("B/401")))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.status",          Matchers.`is`(Status.AVAILABLE.name)))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.address.street",  Matchers.`is`("John's Street")))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.address.city",    Matchers.`is`("Pune")))
             .andExpect(MockMvcResultMatchers.jsonPath("$.address.pinCode", Matchers.`is`("411098")))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.address.state", Matchers.`is`("MH")))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.address.state",   Matchers.`is`("MH")))
     }
 }
